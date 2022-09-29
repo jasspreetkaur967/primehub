74529da4a1d1c2844c20153c08f6680664192e41
jasspreetkaur967 <94992871+jasspreetkaur967@users.noreply.github.com>
2022-09-29 15:13:05 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -10,7 +10,7 @@ for x in range(num1,num2):
     prime = True
     for i in range(2,x):
         if (x%i==0):
-            prime = False
+            prime == False
     if prime == True:
        print( x)
 

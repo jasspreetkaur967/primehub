0249da668667617626c7a48f6f1bec1efa9f6082
jasspreetkaur967 <94992871+jasspreetkaur967@users.noreply.github.com>
2022-09-29 15:56:28 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,18 +1,17 @@
-# Program to check if a number is prime or not
+
 import sys
-num1 = (sys.argv[1])
-num2 = (sys.argv[2])
-num1 = ("Input a number: ")
-num2 = ("Input another number: ")
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
 
+# num1 = int(input('Enter number 1'))
+# num2 = int(input('Enter number 2'))
 
-for x in range(num1,num2):
-    prime = True
-    for i in range(2,x):
-        if (x%i==0):
-            prime == False
-    if prime == True:
-       print( x)
-
-print("Done")
-
+print(f"Prime numbers between {num1} and {num2} are: ")
+for x in range(num1, num2 + 1):
+  count = 0
+  for y in range(2, x//2+1):
+    if (x % y == 0):
+      count = count + 1
+      break
+  if (count == 0 and x != 1):
+    print(f"{x} ", end = ' ')
